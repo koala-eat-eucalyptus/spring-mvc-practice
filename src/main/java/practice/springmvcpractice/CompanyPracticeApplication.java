82798d331749aabcea95dcdeced6154b35764005
Dabeen Jeong <hello70825@gmail.com>
2022-10-14 10:53:50 +0900

    initial setting

---
+++ src/main/java/practice/springmvcpractice/CompanyPracticeApplication.java
@@ -0,0 +1,13 @@
+package practice.springmvcpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/springmvcpractice/CompanyPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.springmvcpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
