37118ebec79b459f5619c0401beecf974cab9895
dener8 <8dener@gmail.com>
2022-10-14 19:50:17 +0900

    modify

--- src/main/java/practice/springmvcpractice/controller/HomeController.java
+++ src/main/java/practice/springmvcpractice/controller/HomeController.java
@@ -5,21 +5,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import practice.springmvcpractice.repository.DependentRepository;
 import practice.springmvcpractice.repository.EmployeeRepository;
 
 @Controller
 public class HomeController {
 
     private final EmployeeRepository employeeRepository;
+    private final DependentRepository dependentRepository;
 
     @Autowired
-    public HomeController(EmployeeRepository employeeRepository) {
+    public HomeController(EmployeeRepository employeeRepository, DependentRepository dependentRepository) {
         this.employeeRepository = employeeRepository;
+        this.dependentRepository = dependentRepository;
     }
+
 
     @RequestMapping("/")
     public String home(Model model) {
-        model.addAttribute("list", employeeRepository.getEmployee("123456789"));
+        model.addAttribute("list", employeeRepository.getAllEmployee());
+        model.addAttribute("list2", dependentRepository.getDependent("333445555", "Joy"));
+
         return "index";
     }
+
+
 }
